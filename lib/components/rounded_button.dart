79d7c7c2dae4cd83d23502729bfb9baebb09254b
Jared Massey <jared@jaredmassey.com>
2020-05-03 11:06:44 -0600

    15.9a

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -25,6 +25,9 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
